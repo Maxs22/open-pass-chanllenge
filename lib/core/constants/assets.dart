7a754e1c01fd23604b682de8a7af92524c50017d
Maximiliano Mendez <maximilianomendez@hotmail.com.ar>
2025-01-10 11:30:47 -0300

    cambios en diseño

--- lib/core/constants/assets.dart
+++ lib/core/constants/assets.dart
@@ -1,4 +1,4 @@
 class ConstantsAssets {
-  static const String logo = 'assets/star_wars_logo.png';
+  static const String logo = 'assets/star_wars_log.png';
   static String infoCircle = 'assets/info-circle.svg';
 }
